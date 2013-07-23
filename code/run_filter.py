05459efca2e17713c3c148bcee5af9cffc5dc33e
Yu-Ju Chen <casbie23@gmail.com>
2013-07-24 05:17:31 +0800

    user can run correct filter or wrong filter

--- code/run_filter.py
+++ code/run_filter.py
@@ -15,10 +15,12 @@ def filter_all(wrong,rows,relation):
 
 if __name__=='__main__':
 	relation=sys.argv[1]
-	wrong=sys.argv[2] #0: correct data; 1:wrong data
+	wrong=int(sys.argv[2]) #0: correct data; 1:wrong data; 2:all data
 	rows=pos_rw.load_json_file(relation)
-	filter_rows=filter_all(wrong,rows,relation)
-
+	if wrong==1 or wrong==0:
+		filter_rows=filter_all(wrong,rows,relation)
+	else:
+		filter_rows=rows
 	#do some analysis of the filtered data or just dump them
 	pos_rw.dump_file( relation , pos_rw.print_format(filter_rows) )
 
